b04eaac3bc9aed4cc2a0de85e29a175ffa3309c2
Paul Brower <brower.paul@gmail.com>
2013-05-08 16:25:17 -0600

    add support for POST multi-part

--- library/src/com/github/browep/httpapi/ApiAdapter.java
+++ library/src/com/github/browep/httpapi/ApiAdapter.java
@@ -1,7 +1,11 @@
 package com.github.browep.httpapi;
 
+import org.apache.http.entity.mime.MultipartEntity;
+
+import java.io.IOException;
 import java.io.InputStream;
 
 public interface ApiAdapter {
     public ApiModel parseToModel(Class clazz, InputStream inputStream);
+    public MultipartEntity toMultipartEntity(ApiMethod apiMethod) throws IOException;
 }
